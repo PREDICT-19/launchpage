b30bccc446b6bf5aada62ebacff541343d6c02a9
Adriaan Knapen <hi@aknapen.nl>
2020-05-07 16:11:42 +0200

    feat(video): replace landing page video and thumbnail

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -79,16 +79,16 @@ const Hero = ({
               </div>
             </div>
           </div>
-          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
+          <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" style={{filter: "brightness(65%)"}} data-reveal-delay="800">
             <a
-              data-video="https://player.vimeo.com/video/174002812"
+              data-video="https://www.youtube-nocookie.com/embed/86AlZklqnmw"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
             >
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                src={require('./../../assets/images/video-placeholder.png')}
                 alt="Hero"
                 width={896}
                 height={504} />
@@ -98,7 +98,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://player.vimeo.com/video/174002812"
+            video="https://www.youtube-nocookie.com/embed/86AlZklqnmw"
             videoTag="iframe" />
         </div>
       </div>
