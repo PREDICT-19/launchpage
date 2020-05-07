e014b73d5d57daba1c9e5aa8b6c9108ff981a24a
Adriaan Knapen <hi@aknapen.nl>
2020-05-07 16:23:17 +0200

    feat(hero): popule hero with predict 19 information

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -61,20 +61,20 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+            PREDICT <span className="text-color-primary">19</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
-                </p>
+                We provide smart contact tracking solutions to prevent dumb lockdowns.
+              </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
-                  <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
-                    </Button>
-                  <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                  <Button tag="a" color="primary" wideMobile href="#more">
+                    Read More
+                  </Button>
+                  <Button tag="a" color="dark" wideMobile href="https://mouroum-sharing.s3.eu-central-1.amazonaws.com/PREDICT-19.apk">
+                    Download (Early Alpha)
+                  </Button>
                 </ButtonGroup>
               </div>
             </div>
